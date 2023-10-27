a0e51e435dc39de75ed8a336b0e5612d9ea89b1b
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-10-27 16:23:53 -0300

    task 01 ok

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,9 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(
+            context["all_files"], key=lambda path: len(path.split("/"))
+        )
         print(f"Deepest file: {deepest_file}")
 
 
